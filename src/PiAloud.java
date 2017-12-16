b638922178c5c011a3295efdaafaea5b8de5dcee
trygamer <league@WTS19.attlocal.net>
2017-12-15 18:30:53 -0800

    Sleepy Head and Own Adventure.io

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
-
+public static void main(String[] args) {
+	
+}
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
 
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -9,20 +9,47 @@ public class SleepyHead {
 
     public static void main(String[] args) {
         
-        boolean isWeekday, isVacation;
+        boolean isWeekday = false, isVacation=false;
         
         /*
          * Ask the user for these values using
          * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
          * JOptionPane.YES_NO_OPTION);
          */
-    
+        int ans=JOptionPane.showConfirmDialog(null, "is it a weekday?", "",JOptionPane.YES_NO_OPTION);
+        
+
+if(ans==0) {
+	isVacation= true;
+	
+}
+
+if(isWeekday==true) {
+	JOptionPane.showMessageDialog(null, "get up lazy bones");
+}
+
+	
+	
 
 
-        /*
+
+int ans1 = 0;
+if(isVacation==true) {
+	  ans1=JOptionPane.showConfirmDialog(null, "is it a vacation?", "",JOptionPane.YES_NO_OPTION);
+	
+}
+if(ans1==0) {
+	JOptionPane.showMessageDialog(null, "sleep in");
+}
+else { 
+	JOptionPane.showMessageDialog(null, "get up lazy bones");
+}
+	
+	/*
+
          * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
          * print “get up lazybones!” If it is a weekday, and we are on vacation,
          * print “sleep in”.
          */
-    }
-}
+    }}
+
