f2103c53f6dc48e0eb39767b15e18b0fcb12b75d
elijahshark <league@WTS19.attlocal.net>
2017-12-22 18:33:04 -0800

    HappyPet.io

--- src/HappyPet.java
+++ src/HappyPet.java
@@ -1,33 +1,60 @@
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
-
 import javax.swing.JOptionPane;
 
 public class HappyPet {
 	// 2. Create a happinessLevel variable to store the pet's happiness number.
-	//    Initialize to zero.
+	// Initialize to zero.
+	static int happinessLevel = 0;
 
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store in variable
-
+	String pet = JOptionPane.showInputDialog("what kind of pet do you want to buy");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
 
-			// 5. Use user input to call the appropriate method created in step 4.
+		// 3. Use showOptionDialog to ask the user what they want to do to make their
+		// pet happy
+		// (eg: cuddle, food, water, take a walk, groom, clean up poop).
+		// Make sure to customize the title and question too.
+		int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your " + pet + " happy?",
+				"Happy Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+				new String[] { "cuddle", "food", "water", "take a walk", "groom", "clean up poop", }, null);
 
-			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
+		// 5. Use user input to call the appropriate method created in step 4.
+if(task==0) {
+	cuddle( pet);
+}
+		// 6. If you determine the happiness level is large enough, tell the
+		// user that he loves his pet and use break; to exit for loop.
 
 	}
 
 	// 4. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
-	//    and INCREMENT the pet's happiness Level.
-}
+	// Each method should create a pop-up with the pet's response (eg. cat might
+	// purr when pet),
+	// and INCREMENT the pet's happiness Level.
+	static void cuddle(String pet) {
+		JOptionPane.showMessageDialog(null, pet + " zzzzzzzzz");
+		happinessLevel =happinessLevel+1 ;
+	}
+
+	static void food(String pet) {
+		JOptionPane.showMessageDialog(null, pet + " yummy");
+		happinessLevel = happinessLevel+3;
+	}
+
+	static void takeawalk(String pet) {
+		JOptionPane.showMessageDialog(null, pet + " zzzzzzzzz");
+		happinessLevel = happinessLevel+2;
+	}
+
+	static void groom(String pet) {
+		JOptionPane.showMessageDialog(null, pet + " purrr");
+		happinessLevel = happinessLevel+1;
+	}
+
+static void cleanuppoop(String pet) {
+JOptionPane.showMessageDialog(null, pet+" ooopsie");
+happinessLevel=happinessLevel+2;
+}}
--- src/OwnAdventure.java
+++ src/OwnAdventure.java
@@ -3,28 +3,34 @@ import javax.swing.JOptionPane;
 public class OwnAdventure {
 
 	public static void main(String[] args) {
-
+boolean f1 =false;
 		String c = JOptionPane.showInputDialog(null, "You entered a dungeon enter a color red, green, blue");
 
 		if (c.equals("red")) {
 			JOptionPane.showMessageDialog(null, " you die from a deadly trap");
+			f1=false;
 		} else if (c.equals("blue")) {
 			JOptionPane.showMessageDialog(null, " You get trampled from the crumbling dungeon");
+			f1=false;
 		}
 
 		else {
-			String f1 = JOptionPane.showInputDialog(null,
-					" you made is succesfully inside the dungeon before it crumbled on you");
+			JOptionPane.showMessageDialog(null,
+					" you made it succesfully inside the dungeon before it crumbled on you");
+			f1=true;
 		}
+		if(f1) {
 		String f=JOptionPane.showInputDialog(null," There are three doors you can choos 1, 2, or 3");
-if(f=("1")) {
+if(f.equals("1")) {
 	JOptionPane.showMessageDialog(null," you meet the boss and die super easily");
 }
-else if(f=("2")) {
+else if(f.equals("2")) {
 	JOptionPane.showMessageDialog(null," you find treasure and get teleported out of the dungeon; YOU WIN!");
 }
 else { JOptionPane.showMessageDialog(null," you fall into an endless pit");
 
 }
+		}
 	}
-}
+	}
+
